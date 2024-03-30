e0be5859e4f44db05cc034901397a4d94b960652
Brett Baker <GitBaBB@MyBaBB.com>
2024-03-30 14:01:14 -0700

    working on the custom navbar for the branding chute conversion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,21 +23,19 @@ const [isMenuClicked, setIsMenuClicked] = useState(false)
     setIsMenuClicked(!isMenuClicked)
  }
 
-  return (
+return (
     <div style={{ width: '100%', height: '100vh' }}>
         <nav>
-        <div className='burger-menu' onClick={updateMenu}>
-            <div className={burger_class}  ></div>
-            <div className={burger_class}  ></div>
-            <div className={burger_class}  ></div>
-        </div>    
-        </nav>        
+            <div className='burger-menu sm:hidden' onClick={updateMenu} >
+                <div className={burger_class} style={{ backgroundColor: 'red', boxShadow: 'inset 0 0 5px rgba(0, 0, 0, 0.5)' }}></div>
+                <div className={burger_class} style={{ backgroundColor: 'white', boxShadow: 'inset 0 0 5px rgba(0, 0, 0, 0.5)' }}></div>
+                <div className={burger_class} style={{ backgroundColor: 'Blue', boxShadow: 'inset 0 0 5px rgba(0, 0, 0, 0.5)' }}></div>
+            </div>
+        </nav>
 
-
-<div className={menu_class}></div>
-
+        <div className={menu_class}></div>
     </div>
-  )
+)
 }
 
 export default Navbar
